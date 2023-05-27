687545189a2e5d67529a60a0504105261e4ed97d
danish wadhwa <danish.wadhwa1997@gmail.com>
2023-05-27 14:31:52 +0530

    updated app stack with rooms and hostel

--- Components/Navigation/AppStack.tsx
+++ Components/Navigation/AppStack.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Hostels from '../Hostels/index';
 import Profile from '../Profile/Profile';
+import Hostel from '../Hostels/Hostel';
+import RoomsContainer from '../Room/RoomsContainer';
+import Room from '../Room/Room';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +12,21 @@ const AppStack = () => {
   return (
     <Stack.Navigator initialRouteName="Hostels">
       <Stack.Screen name="Hostels" component={Hostels} />
+      <Stack.Screen
+        name="Hostel"
+        component={Hostel}
+        initialParams={{hostelId: ''}}
+      />
+      <Stack.Screen
+        name="Rooms"
+        component={RoomsContainer}
+        initialParams={{hostelId: ''}}
+      />
+      <Stack.Screen
+        name="Room"
+        component={Room}
+        initialParams={{hostelId: '', roomNumber: ''}}
+      />
       <Stack.Screen name="Profile" component={Profile} />
     </Stack.Navigator>
   );
