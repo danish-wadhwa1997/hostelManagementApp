fb30b44c63019b65c6487e4883a1b67be9ad2de1
danish wadhwa <danish.wadhwa1997@gmail.com>
2023-05-28 11:56:01 +0530

    hostel list done

---
+++ Components/Hostels/HostelCard.tsx
@@ -0,0 +1,92 @@
+import {StyleSheet, View, Image} from 'react-native';
+import {Text, Button, Surface, TouchableRipple} from 'react-native-paper';
+import React from 'react';
+import {HostelType} from './index';
+import {RFValue} from 'react-native-responsive-fontsize';
+import {useNavigation} from '@react-navigation/native';
+
+type Props = {hostel: HostelType};
+
+const HostelCard = ({hostel}: Props) => {
+  const navigation = useNavigation();
+
+  const handleClick = () => {
+    navigation.navigate('Rooms', {hostelId: hostel.id});
+  };
+
+  const handleViewHostelDetails = () => {
+    navigation.navigate('Hostel', {hostelId: hostel.id});
+  };
+
+  return (
+    <Surface elevation={2} style={styles.surface}>
+      <TouchableRipple onPress={handleViewHostelDetails}>
+        <View style={styles.container}>
+          <Image
+            source={{uri: 'https://picsum.photos/700'}}
+            style={styles.image}
+          />
+          <View style={styles.detailsContainer}>
+            <View>
+              <Text style={styles.title}>{hostel.name}</Text>
+              <Text style={styles.description}>{hostel.address}</Text>
+            </View>
+            <View style={styles.buttonRatingContainer}>
+              <Text style={styles.rating}>{hostel.rating} / 5</Text>
+              <Button
+                icon="arrow-right"
+                mode="text"
+                contentStyle={styles.btnContentStyle}
+                onPress={handleClick}>
+                Rooms
+              </Button>
+            </View>
+          </View>
+        </View>
+      </TouchableRipple>
+    </Surface>
+  );
+};
+
+export default HostelCard;
+
+const styles = StyleSheet.create({
+  surface: {margin: RFValue(10), borderRadius: RFValue(8)},
+  container: {
+    flexDirection: 'row',
+  },
+  image: {
+    width: '50%',
+    aspectRatio: 1, // Ensures the image maintains its aspect ratio
+    borderRadius: RFValue(8),
+    borderBottomRightRadius: 0,
+    borderTopRightRadius: 0,
+  },
+  detailsContainer: {
+    flex: 1,
+    padding: RFValue(10),
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: RFValue(16),
+    fontWeight: 'bold',
+    marginBottom: RFValue(5),
+  },
+  description: {
+    fontSize: RFValue(14),
+    // color: ,
+  },
+  rating: {
+    fontSize: RFValue(12),
+    // color: '#888',
+  },
+  buttonRatingContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    columnGap: RFValue(10),
+  },
+  btnContentStyle: {
+    flexDirection: 'row-reverse',
+  },
+});
--- Components/Hostels/index.tsx
+++ Components/Hostels/index.tsx
@@ -1,13 +1,106 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, SafeAreaView, FlatList, View} from 'react-native';
+import React, {useEffect, useState, useMemo} from 'react';
+import {getAllHostels} from '../Services/hostelServices';
+import {RFValue} from 'react-native-responsive-fontsize';
+import HostelCard from './HostelCard';
+import {IconButton, TextInput} from 'react-native-paper';
 
-const index = () => {
+export type HostelType = {
+  address: string;
+  adminId: string;
+  'date-created': string;
+  'date-modified': string;
+  description: string;
+  id: string;
+  rating: string;
+  reviews: Array<string>;
+  rules: Array<string>;
+  name: string;
+};
+let allHostels: Array<HostelType>;
+
+const HostelsContainer = () => {
+  const [hostels, setHostels] = useState<Array<HostelType>>();
+  const [searchQuery, setSearchQuery] = useState<string>();
+
+  const filterHostelsBySearch = (value: string) => {
+    if (value) {
+      return allHostels.filter((hostel: HostelType) =>
+        hostel.name.toLowerCase().includes(value?.toLowerCase()),
+      );
+    } else {
+      return allHostels;
+    }
+  };
+
+  const handleSearch = (value: string) => {
+    setSearchQuery(value);
+    setHostels(filterHostelsBySearch(value));
+  };
+
+  const getAllBuildings = async () => {
+    try {
+      const response = await getAllHostels();
+      if (response?.data?.buildings) {
+        allHostels = response.data.buildings;
+        setHostels(response.data.buildings);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllBuildings();
+  }, []);
   return (
-    <View style={{height: '100%'}}>
-      <Text>index</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.filtersContainer}>
+        <TextInput
+          value={searchQuery}
+          onChangeText={handleSearch}
+          mode="outlined"
+          placeholder="search"
+          dense
+          style={styles.searchStyle}
+        />
+        <IconButton
+          icon="tune"
+          style={styles.filterIcon}
+          size={RFValue(25)}
+          onPress={() => console.log('Pressed')}
+        />
+      </View>
+      <FlatList
+        data={hostels}
+        renderItem={({item}) => <HostelCard hostel={item} />}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
   );
 };
 
-export default index;
-const styles = StyleSheet.create({});
+export default HostelsContainer;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: RFValue(10),
+    paddingBottom: RFValue(20),
+    paddingTop: RFValue(5),
+  },
+  filtersContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    columnGap: RFValue(5),
+    paddingBottom: RFValue(5),
+  },
+  searchStyle: {
+    flexGrow: 1,
+  },
+  filterIcon: {
+    borderRadius: RFValue(5),
+    flexBasis: '10%',
+  },
+});
