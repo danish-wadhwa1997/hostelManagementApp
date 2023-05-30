664b9e0cd9e4d46bd309a7d9a346e4987836a3ba
danish wadhwa <danish.wadhwa1997@gmail.com>
2023-05-30 23:14:28 +0530

    added logout to profile

--- Components/Profile/Profile.tsx
+++ Components/Profile/Profile.tsx
@@ -1,10 +1,26 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import React, {useContext} from 'react';
+import {Button} from 'react-native-paper';
+import {logoutUser} from '../Services/loginService';
+import {AuthContext} from '../Context/AuthContext';
 
 const Profile = () => {
+  const {setUserAuthorized} = useContext(AuthContext);
+  const logout = async () => {
+    try {
+      const userLoggedOut = await logoutUser();
+      if (userLoggedOut) {
+        setUserAuthorized(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setUserAuthorized(false);
+    }
+  };
+
   return (
     <View>
-      <Text>Profile</Text>
+      <Button onPress={logout}>Logout</Button>
     </View>
   );
 };
