b7ad4cb389ffbd505807d8f7d9131dcd38a01660
danish wadhwa <danish.wadhwa1997@gmail.com>
2023-05-30 23:14:13 +0530

    added profile to header

--- Components/Navigation/AppStack.tsx
+++ Components/Navigation/AppStack.tsx
@@ -5,12 +5,26 @@ import Profile from '../Profile/Profile';
 import Hostel from '../Hostels/Hostel';
 import RoomsContainer from '../Room/RoomsContainer';
 import Room from '../Room/Room';
+import {IconButton, useTheme} from 'react-native-paper';
 
 const Stack = createNativeStackNavigator();
 
 const AppStack = () => {
+  const theme = useTheme();
   return (
-    <Stack.Navigator initialRouteName="Hostels">
+    <Stack.Navigator
+      initialRouteName="Hostels"
+      screenOptions={({navigation}) => ({
+        // eslint-disable-next-line react/no-unstable-nested-components
+        headerRight: () => (
+          <IconButton
+            icon="account-outline"
+            iconColor={theme.colors.onPrimary}
+            size={30}
+            onPress={() => navigation.navigate('Profile')}
+          />
+        ),
+      })}>
       <Stack.Screen name="Hostels" component={Hostels} />
       <Stack.Screen
         name="Hostel"
